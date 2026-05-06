c491dfb83de290321fa1a21d46fdcb8b4dd357fa
fvautomation <joel@fvautomation.ca>
2026-05-05 18:53:26 -0700

    assignment_1 task_2

---
+++ Assignment_1/Task2.java
@@ -0,0 +1,8 @@
+public class Task2{
+    public static void main(String[] args) {
+        String s1 = "135";
+        String s2 = "215";
+
+        System.out.println("Sum of " + s1 + " and " + s2 + "is: " + (Integer.valueOf(s1) + Integer.valueOf(s2)));
+    }
+}
