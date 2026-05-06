274b4a34104489c2b1f58cd6183365eb3e17b14d
fvautomation <joel@fvautomation.ca>
2026-05-05 18:50:26 -0700

    assignment_1 task_1

---
+++ Assignment_1/Task1.java
@@ -0,0 +1,10 @@
+public class Task1{
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello, group 2");
+        sb.append("! Welcome ");
+        sb.append("to COMP ");
+        sb.append(251);
+        sb.append(" course.");
+        System.out.println(sb);
+    }
+}
