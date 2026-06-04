8b1876dbc12f65bf841dc7901cee7fccab64ee90
fvautomation <joel@fvautomation.ca>
2026-06-04 07:39:13 -0700

    assignment 7 files added

--- Assignment_6/task1.java
+++ Assignment_6/task1.java
@@ -12,7 +12,29 @@ class task1{
         Position<Integer> rightChild = binaryTree.addRight(root, 3);
 
         //your code should come here
-        System.out.println(binaryTree.root().getElement());
+        Position<Integer> p1 = binaryTree.addLeft(leftChild, 4);
+        Position<Integer> p2 = binaryTree.addRight(rightChild, 5);
+        Position<Integer> p3 = binaryTree.addLeft(p2, 6);
+        Position<Integer> p4 = binaryTree.addRight(p2, 7);
 
+        System.out.println("Binary Tree Structure");
+
+        Iterable<Position<Integer>> array = binaryTree.breadthfirst();
+
+        System.out.println("Root: " + binaryTree.root.getElement());
+        System.out.println("Left Child of Root: " + binaryTree.root.getLeft().getElement());
+        System.out.println("Right Child of Root: " + binaryTree.root.getRight().getElement());
+        
+
+        getNext(binaryTree, root);
+
+        for (Position a: array){
+            System.out.println(a.getElement());
+        }
+
+    }
+
+    static void getNext(LinkedBinaryTree<Integer> tree, Position<Integer> parent){
+        System.out.println(parent.getElement());
     }
 }
